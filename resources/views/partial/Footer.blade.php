537be11e61e0146987cefda182f5c3af3ebf0387
Ali Mohamed Ali <am6064029@gmail.com>
2020-02-02 10:44:37 +0200

    some details

--- resources/views/partial/Footer.blade.php
+++ resources/views/partial/Footer.blade.php
@@ -1,9 +1,47 @@
-<!-- Footer -->
-  <footer class="bg-light py-5">
-    <div class="container">
-      <div class="small text-center text-muted">Copyright &copy; 2019 - Start Bootstrap</div>
+  <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://twitter.com/">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/alkrnaval.ali.75/">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+
+<li class="list-inline-item">
+              <a href="https://github.com/alimohamedac/">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+<li class="list-inline-item">
+              <a href="https://www.linkedin.com/feed/?trk=onboarding-landing/">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-linkedin-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+
+
+          </ul>
+          <div class="small text-center text-muted">Copyright &copy; 2020 - Ali Mohamed AC</div>
+        </div>
+      </div>
     </div>
-  </footer>
 
   <!-- Bootstrap core JavaScript -->
   <script src="{{ asset('themes/vendor/jquery/jquery.min.js') }}"></script>
